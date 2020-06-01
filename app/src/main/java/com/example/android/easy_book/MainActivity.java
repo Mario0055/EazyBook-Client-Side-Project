fee7774924e27852f65d981dfe775550072839fb
iammayar <mayargamal497@gmail.com>
2020-06-01 21:26:29 +0200

    completing home page

--- app/src/main/java/com/example/android/easy_book/MainActivity.java
+++ app/src/main/java/com/example/android/easy_book/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.android.easy_book;
 
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
 }
